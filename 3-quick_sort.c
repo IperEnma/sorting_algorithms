9f93ce3d25b540f2199bf4a5fd11c9ff76fad83b
IperENma <enmanuelhernandez1843@gmail.com>
2022-06-12 16:33:10 -0700

    sorting

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -8,15 +8,15 @@
  */
 void swap(int *array, int *swap_element1, int *swap_element2, size_t len)
 {
-	    int temp = 0;
-    
-        if (*swap_element1 != *swap_element2)
-    {
-        temp = *swap_element1;
-	    *swap_element1 = *swap_element2;
-	    *swap_element2 = temp;
-        print_array(array, len);
-    }
+	int temp = 0;
+	
+	if (*swap_element1 != *swap_element2)
+	{
+		temp = *swap_element1;
+		*swap_element1 = *swap_element2;
+		*swap_element2 = temp;
+		print_array(array, len);
+	}
 }
 /**
  * quick_sort_suplement - suplement recursive function
@@ -28,40 +28,40 @@ void swap(int *array, int *swap_element1, int *swap_element2, size_t len)
  */
 void quick_sort_suplement(int *array, size_t first, size_t last, size_t len)
 {
-        size_t slow = 0, fast = 0, pivot, flag = 0;
-
-    if (first < last)
-    {
-	    if ((last) - first <= 0)
-		    return;
-	    if (last == 1 && array[0] < array[last])
-		    return;
-
-	    fast = slow = first;
-	    pivot = last;
-	    while (fast < pivot)
-	    {
-		    if (array[fast] < array[pivot])
-		    {
-                flag = 1;
-			    swap(array, &array[fast], &array[slow], len);
-			    slow++;
-		        }
-		    fast++;
-	    }
-	    swap(array, &array[slow], &array[pivot], len);
-    
-        if (flag == 1)
-        {
-		    quick_sort_suplement(array, slow + 1, last, len);
-		    quick_sort_suplement(array, first, slow - 1, len);
-        }
-        else
-        {
-            quick_sort_suplement(array, slow, last, len);
-            quick_sort_suplement(array, first, slow, len);
-        }
-    }
+	size_t slow = 0, fast = 0, pivot, flag = 0;
+	
+	if (first < last)
+	{
+		if ((last) - first <= 0)
+			return;
+		if (last == 1 && array[0] < array[last])
+			return;
+		
+		fast = slow = first;
+		pivot = last;
+		while (fast < pivot)
+		{
+			if (array[fast] < array[pivot])
+			{
+				flag = 1;
+				swap(array, &array[fast], &array[slow], len);
+				slow++;
+			}
+			fast++;
+		}
+		swap(array, &array[slow], &array[pivot], len);
+		
+		if (flag == 1)
+		{
+			quick_sort_suplement(array, slow + 1, last, len);
+			quick_sort_suplement(array, first, slow - 1, len);
+		}
+		else
+		{
+			quick_sort_suplement(array, slow, last, len);
+			quick_sort_suplement(array, first, slow, len);
+		}
+	}
 }
 /**
  * quick_sort - sorting quick init
@@ -71,5 +71,6 @@ void quick_sort_suplement(int *array, size_t first, size_t last, size_t len)
  */
 void quick_sort(int *array, size_t size)
 {
-	quick_sort_suplement(array, 0, size - 1, size);
+	if(array && size > 1)
+		quick_sort_suplement(array, 0, size - 1, size);
 }
