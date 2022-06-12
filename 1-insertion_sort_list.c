6fadf124fb8338c68c79d91ec2fc1e2f219a00eb
IperENma <enmanuelhernandez1843@gmail.com>
2022-06-12 07:09:12 -0700

    sorting

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,10 +8,9 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *ahead = NULL, *node = NULL;
 
-	ahead = *list;
-
 	if (*list == NULL || list == NULL)
 		return;
+	ahead = *list;
 	while (ahead)
 	{
 		while ((ahead->prev) && (ahead->n < ahead->prev->n))
