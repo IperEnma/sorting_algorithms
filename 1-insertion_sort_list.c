efbf98d7ab036b98af2d8497750e6669a9ddabf4
IperENma <enmanuelhernandez1843@gmail.com>
2022-06-12 07:04:08 -0700

    sorting

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -6,28 +6,29 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *ahead = NULL, *node = NULL;
-    
-    ahead = *list;
+	listint_t *ahead = NULL, *node = NULL;
 
-    while(ahead)
-    {
-        while ((ahead->prev) && (ahead->n < ahead->prev->n))
-        {
-            node = ahead->prev;
+	ahead = *list;
 
-            if (ahead->prev->prev)
-                ahead->prev->prev->next = ahead;
-            ahead->prev = ahead->prev->prev;
-            node->next = ahead->next;
-            node->prev = ahead;
-            if (ahead->next)
-                ahead->next->prev = node;
-            ahead->next = node;
-            if (ahead->prev == NULL)
-                *list = ahead;
-            print_list(*list);
-        }
-        ahead = ahead->next;
-    }
+	if (*list == NULL)
+		return;
+	while (ahead)
+	{
+		while ((ahead->prev) && (ahead->n < ahead->prev->n))
+		{
+			node = ahead->prev;	
+			if (ahead->prev->prev)
+				ahead->prev->prev->next = ahead;
+			ahead->prev = ahead->prev->prev;
+			node->next = ahead->next;
+			node->prev = ahead;
+			if (ahead->next)
+				ahead->next->prev = node;
+			ahead->next = node;
+			if (ahead->prev == NULL)
+				*list = ahead;
+			print_list(*list);
+		}
+		ahead = ahead->next;
+	}
 }
