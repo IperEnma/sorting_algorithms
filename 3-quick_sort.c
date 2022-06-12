26c48189588db701cf825b6b721b0156d5a6b94e
IperEnma <enmanuelhernandez1843@gmail.com>
2022-06-12 20:26:19 -0300

    sorting

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -8,12 +8,15 @@
  */
 void swap(int *array, int *swap_element1, int *swap_element2, size_t len)
 {
-	int temp = 0;
-	
-	temp = *swap_element1;
-	*swap_element1 = *swap_element2;
-	*swap_element2 = temp;
-    print_array(array, len);
+	    int temp = 0;
+    
+        if (*swap_element1 != *swap_element2)
+    {
+        temp = *swap_element1;
+	    *swap_element1 = *swap_element2;
+	    *swap_element2 = temp;
+        print_array(array, len);
+    }
 }
 /**
  * quick_sort_suplement - suplement recursive function
@@ -25,29 +28,40 @@ void swap(int *array, int *swap_element1, int *swap_element2, size_t len)
  */
 void quick_sort_suplement(int *array, size_t first, size_t last, size_t len)
 {
-	size_t slow = 0, fast = 0, pivot;
+        size_t slow = 0, fast = 0, pivot, flag = 0;
 
-	if ((last) - first <= 0)
-		return;
-	if (last == 1 && array[0] < array[last])
-		return;
+    if (first < last)
+    {
+	    if ((last) - first <= 0)
+		    return;
+	    if (last == 1 && array[0] < array[last])
+		    return;
 
-	slow = first; 
-    fast = first;
-	pivot = last;
-	while (fast <= pivot)
-	{
-		if (array[fast] <= array[pivot])
-		{
-			swap(array, &array[slow], &array[fast], len);
-			slow++;
-		}
-		fast++;
-	}
-	if (last - first > 0)
-		quick_sort_suplement(array, slow + 1, last, len);
-	if (last - first > 0)
-		quick_sort_suplement(array, first, slow, len);
+	    fast = slow = first;
+	    pivot = last;
+	    while (fast < pivot)
+	    {
+		    if (array[fast] < array[pivot])
+		    {
+                flag = 1;
+			    swap(array, &array[fast], &array[slow], len);
+			    slow++;
+		        }
+		    fast++;
+	    }
+	    swap(array, &array[slow], &array[pivot], len);
+    
+        if (flag == 1)
+        {
+		    quick_sort_suplement(array, slow + 1, last, len);
+		    quick_sort_suplement(array, first, slow - 1, len);
+        }
+        else
+        {
+            quick_sort_suplement(array, slow, last, len);
+            quick_sort_suplement(array, first, slow, len);
+        }
+    }
 }
 /**
  * quick_sort - sorting quick init
