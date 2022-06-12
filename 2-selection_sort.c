fa87ee7bf56d270488c9769af463895745999f31
IperENma <enmanuelhernandez1843@gmail.com>
2022-06-12 08:26:00 -0700

    sorting

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,39 +1,41 @@
 #include "sort.h"
 /**
- *
- *
+ * selection_sort - sorting array
+ * @array: array
+ * @size: size array
+ * Return: void
  */
 void selection_sort(int *array, size_t size)
 {
-    size_t i = 0, j = 0, position = 0;
-    int temp = 0, flag = 0, value = 0;
-    
-    for (i = 0; i < size; i++)
-    {
-        flag = 0;
-        for (j = i + 1; j < size; j++)
-        {
-            if (flag)
-            {
-                if(array[position] > array[j])
-                {
-                    position = j;
-                    value = array[j];
-                }
-            }
-            else if (array[i] > array[j])
-            {
-                flag = 1;
-                position = j;
-                value = array[j];
-            }
-        }
-        if (flag)
-        {
-            temp = array[i];
-            array[i] = value;
-            array[position] = temp;
-            print_array(array, size);
-        }
-    }
+	size_t i = 0, j = 0, position = 0;
+	int temp = 0, flag = 0, value = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		flag = 0;
+		for (j = i + 1; j < size; j++)
+		{
+			if (flag)
+			{
+				if (array[position] > array[j])
+				{
+					position = j;
+					value = array[j];
+				}
+			}
+			else if (array[i] > array[j])
+			{
+				flag = 1;
+				position = j;
+				value = array[j];
+			}
+		}
+		if (flag)
+		{
+			temp = array[i];
+			array[i] = value;
+			array[position] = temp;
+			print_array(array, size);
+		}
+	}
 }
